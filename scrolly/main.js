cca505b24d5cf5b4855d40dec9656c2a60ca5d5e
Anatolii Nehelia <a.negelya@windowslive.com>
2018-03-19 12:43:39 +0200

    forEach bug fixed

--- scrolly/main.js
+++ scrolly/main.js
@@ -6,29 +6,48 @@ var scenes = [],
 function createScenes(element) {
     var childs = element.querySelectorAll(".scene");
 
-    var i = 0;
-    childs.forEach(function(child) {
-        var scene = {};
-        scene['content'] = child;
-        scene['coord'] = visibleY*i;
-        i+=2;
+    var j = 0;
 
-        child.style.transform = "translateY(" + scene['coord'] + "px)";
+    for(var i=0; i < childs.length; i++) {
+        var scene = {};
+        scene['content'] = childs[i];
+        scene['coord'] = visibleY*j;
+        j+=2;
+
+        childs[i].style.transform = "translateY(" + scene['coord'] + "px)";
 
         scenes.push(scene);
-    });
+    }
+    // childs.forEach(function(child) {
+    //     var scene = {};
+    //     scene['content'] = child;
+    //     scene['coord'] = visibleY*i;
+    //     i+=2;
 
-    document.body.style.height = (visibleY * i) + "px";
+    //     child.style.transform = "translateY(" + scene['coord'] + "px)";
+
+    //     scenes.push(scene);
+    // });
+
+    document.body.style.height = (visibleY * j) + "px";
 }
 
 function animateScenes() {
     scrollY = window.pageYOffset;
 
-    scenes.forEach(function(scene){
-        if(scrollY <= scene["coord"]) {
-            scene["content"].style.transform = "translateY(" + (scene["coord"] - scrollY) + "px)";
-        } else if (scrollY >= (scene["coord"] + visibleY)) {
-            scene["content"].style.transform = "translateY(" + (scene["coord"] - (scrollY - visibleY)) + "px)";
+    for(var i = 0; i < scenes.length; i++) {
+        if(scrollY <= scenes[i]["coord"]) {
+            scenes[i]["content"].style.transform = "translateY(" + (scenes[i]["coord"] - scrollY) + "px)";
+        } else if (scrollY >= (scenes[i]["coord"] + visibleY)) {
+            scenes[i]["content"].style.transform = "translateY(" + (scenes[i]["coord"] - (scrollY - visibleY)) + "px)";
         }
-    });
+    }
+
+    // scenes.forEach(function(scene){
+    //     if(scrollY <= scene["coord"]) {
+    //         scene["content"].style.transform = "translateY(" + (scene["coord"] - scrollY) + "px)";
+    //     } else if (scrollY >= (scene["coord"] + visibleY)) {
+    //         scene["content"].style.transform = "translateY(" + (scene["coord"] - (scrollY - visibleY)) + "px)";
+    //     }
+    // });
 }
